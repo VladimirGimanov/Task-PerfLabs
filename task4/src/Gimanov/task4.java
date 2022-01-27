bb4f7cc25e89a5d42a697aed2d456ed679a4dd05
VladimirGimanov <98458604+VladimirGimanov@users.noreply.github.com>
2022-01-28 00:01:01 +0300

    Add files via upload

---
+++ task4/src/Gimanov/task4.java
@@ -0,0 +1,34 @@
+package Gimanov;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class task4 {
+    public static void main(String[] args) throws IOException {
+        int sum = 0;
+        int r = 0;
+        int tmp;
+        String line = "";
+        File file = new File(args[0]);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        List<Integer> list = new ArrayList<>();
+
+        while((line = reader.readLine()) != null) {
+            tmp = Integer.parseInt(line);
+            list.add(tmp);
+            sum+=tmp;
+        }
+
+        sum = sum/ list.size();
+        for(int j = 0;j <list.size(); j++) {
+            r += Math.abs(sum - list.get(j));
+        }
+        System.out.println(r);
+
+    }
+}
+
